51b518817083fa6d5888de6d081ac5b8aed32ee3
juliana <nani.jimenez@hotmail.com>
2020-09-13 22:57:40 -0500

    edit

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -9,7 +9,7 @@ export const elementosPost = async () => {
     </nav>
               <div class="TL" id="muro"> 
                   <form class ="form" id = "text-post">
-                      <input class= "text" type="text" id = "input-text" placeholder="write here">
+                  <input class= "text" type="text" id = "input-text" placeholder="write here">
                 
                   <button id="b-post" type="submit">comment</button>       
               </form>
@@ -18,9 +18,7 @@ export const elementosPost = async () => {
 
               <a href = '#/time-line' class="time-line"> </a>
               </div> 
-              </div>`
-    //variable evento boton comentar
-    
+              </div>`  
 //funcion y evento de salir*/
 
     let out = divElement.querySelector('.outb');
@@ -40,10 +38,12 @@ const savePost = (description, email) =>   //Guarda comentarios en datos de fire
     email : email,
     likes : []
    })
-    
+
+   let editStatus = false;
+   let id = "";
 
 const likePost = async (currentUserId, postId, pushLike) => {
-      const postRef = data.collection('post').doc(postId);
+      const postRef = data.collection('posts').doc(postId);
       if (pushLike) {
         postRef.update({
           likes: firebase.firestore.FieldValue.arrayRemove(currentUserId),
@@ -59,34 +59,34 @@ const likePost = async (currentUserId, postId, pushLike) => {
 export const afterloading = async () =>{
   let onGetTask = (callback) => data.collection('posts').onSnapshot(callback) 
   let deletePost = id => data.collection('posts').doc(id).delete() 
-  let btnlike = document.querySelector('#like-id')
-  //console.log("btnlike")
-
+  let getEdit = (id) => data.collection('posts').doc(id).get();
+  let updateEdit = (id, updateEdit) => data.collection('posts').doc(id).update//var. actualiza el post editado
+ (updateEdit);
   let PostsContainer = document.querySelector('.p');
   let commentBtn = document.querySelector('#b-post');
-  let formulario = document.querySelector("#text-post")
-  console.log(formulario)
+  let formulario = document.querySelector("#text-post");
+  //console.log(formulario)
    
   
   onGetTask((querySnapshot)=>{
     //console.log('intentando cambios');
     PostsContainer.innerHTML=""
     querySnapshot.forEach(doc => {
-      let contador = 0
       let post = doc.data() 
       post.id = doc.id
       PostsContainer.innerHTML += `<div class ="tarjeta"> <a href = "" class="user-name"> 
       ${post.email}  </a>
       <h1> ${post.description} </h1> 
-        <div>  <button class="b-edith">Edith</button>
+        <div>  <button class="b-edit" data-id="${post.id}">Edit</button>
         <button class="b-delete" id="post-id" data-id="${post.id}" data-email="${post.email}">Delete</button>
-        <button class="blike" id="like-id" data-id="${post.id}"  > &#129505
-          <h2 id ="numero"> "${contador}" </h2>
+        <button class="blike" id="like-id" data-id="${post.id}"  > &#129505 ${post.likes.length}</button>
+          <h2 id ="numero"> "${post.likes.length}" </h2>
         </button>
 
         </div>
         </div>`
       const btnsDelete = document.querySelectorAll('.b-delete')
+      //console.log(btnsDelete)
       btnsDelete.forEach(btn =>{ 
         btn.addEventListener('click', async(e)=>{
           //console.log(e.target.dataset.id)
@@ -95,22 +95,49 @@ export const afterloading = async () =>{
          
           if( e.target.dataset.email === user.email ){ 
           await deletePost(e.target.dataset.id)}
-
         })
       })
+//funcion likes
+      const btnsLike = document.querySelectorAll('.blike')
+      //console.log(btnsLike)
+      let user = firebase.auth().currentUser;
+      let pushLike = post.likes.some(likes => likes === user.uid);
 
-      const btnlike = document.querySelectorAll('.blike')
-      
-      //console.log(contador)
-      btnlike.forEach(like => {
+      btnsLike.forEach(like => {
+        //console.log(like)
         like.addEventListener('click', async(m) => { 
-          console.log(m.target)
-          /*contador = contador + 1 ; 
-          document.getElementById("numero").innerHTML = contador
-          console.log(contador)*/
+          let idlikes = m.target.dataset.id
+          let user = firebase.auth().currentUser;
+          await likePost(user.uid, idlikes, pushLike);
+          if (pushLike) {
+            pushLike = false;
+          } else {
+            pushLike = true;
+          }
+
 
         })
       })
+
+    //funcion editar
+  
+    const btnedit = document.querySelectorAll('.b-edit')
+    btnedit.forEach(btn => {
+      btn.addEventListener('click', async (e) => {
+        //console.log(e.target.dataset.id)
+        let doc = await getEdit(e.target.dataset.id);
+        let task = doc.data ();
+        editStatus = true ;
+        id = doc.id;
+
+
+        console.log(doc.data())
+        console.log(formulario["input-text"])
+        formulario['input-text'].value = task.description
+        formulario['b-post'].innerText = 'update' 
+      })
+    })
+    
   })
      
   commentBtn.addEventListener('click', async(event) => {
@@ -120,11 +147,27 @@ export const afterloading = async () =>{
     //console.log(description)
     let user = firebase.auth().currentUser;
     //console.log(user.email, user.uid, user.emailVerified);
+    if (!editStatus){ 
+
     await savePost(description,user.email) 
+  } else {
+    await updateEdit(id, {
+      //title: user.value,
+      description: description
+    })
+
+    editStatus = false;
+    id='';
+    formulario['b-post'].innerText = 'Publicar';
+
+  }
+
+  //await getEdit();
     formulario.reset()
   })
+
 })
-    }
+}
   
   /*
 
